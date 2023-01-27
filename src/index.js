622cc4726023ff927fb948d56b9d53505f03694f
Alexey10031994 <alexseva94@gmail.com>
2023-01-27 16:05:52 +0200

    connected api marvel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MarvelService from './services/MarvelService';
 
 import App from './App';
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
