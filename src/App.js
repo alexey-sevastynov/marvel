df14b9107ede0d47f9024a465c8c445f265be097
Alexey10031994 <alexseva94@gmail.com>
2023-01-27 13:07:10 +0200

    added component randomChar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from "./components/Header";
+import RandomChar from "./components/RandomChar";
 
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <main>
+
+        <RandomChar />
+      </main>
     </div>
   );
 }
