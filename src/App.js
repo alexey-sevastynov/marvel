cbb8995a5449fa0a50bb3add5f06837b27d002f8
Alexey10031994 <alexseva94@gmail.com>
2023-01-27 13:21:32 +0200

    added component CharInfo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import CharInfo from "./components/CharInfo";
+import CharList from "./components/CharList";
 import Header from "./components/Header";
 import RandomChar from "./components/RandomChar";
+
+import decoration from './resources/img/vision.png';
 
 
 function App() {
@@ -10,6 +14,13 @@ function App() {
       <main>
 
         <RandomChar />
+
+        <div className="char__content">
+          <CharList />
+          <CharInfo />
+        </div>
+
+        <img className="bg-decoration" src={decoration} alt="vision" />
       </main>
     </div>
   );
