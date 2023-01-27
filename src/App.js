ae11f78dc99d9ebf5e8b313b90336dea08e2547d
Alexey10031994 <alexseva94@gmail.com>
2023-01-27 12:30:52 +0200

    added folder "resources", and fill it with images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     
+      Start
     </div>
   );
 }
 
 export default App;
+// added folder "resources" , continued fill a foto
